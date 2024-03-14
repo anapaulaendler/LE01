e23ac28dc31aa55cd073142ff1056a3cb04380eb
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-14 13:27:22 -0300

    Sexto Exercício

---
+++ ExercicioSeis.java
@@ -0,0 +1,18 @@
+public class ExercicioSeis {
+
+    public static void executar() {
+
+        /* Ler uma temperatura em graus Celsius e apresentá-la convertida em graus
+        Fahrenheit. A fórmula de conversão é: F = (9 * C + 160) /  5 */
+
+        double c = Prompt.lerDecimal("Digite a temperatura em Celsius: ");
+
+        double f = (9 * c + 160) / 5;
+
+        System.out.printf("A temperatura %f graus Celsius é igual a %f graus Fahrenheit.\n", c, f);
+
+        Prompt.separador();
+        
+    }
+
+}
