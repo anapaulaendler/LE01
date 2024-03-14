d8717ad413b3ea8870e2d09e281b7114d5a29296
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-14 13:30:03 -0300

    Exercício Doze

---
+++ ExercicioDoze.java
@@ -0,0 +1,19 @@
+public class ExercicioDoze {
+
+    public static void executar() {
+
+        /* Escreva um algoritmo que receba o número do mês e mostre o mês
+        correspondente. Valide mês inválido */
+
+        int a = Prompt.lerInteiro("Digite o número do mês: ");
+        String[] nome = new String[12];
+
+        nome = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+        if (a < 1 && a > 12) {
+            System.out.println("Número inválido!");
+        } else {
+            System.out.println(nome[a - 1]);
+        }
+    }
+}
