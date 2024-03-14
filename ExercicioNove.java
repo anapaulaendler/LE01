ae54cb6f83b7265d3fe30ec83faa448d9a05e256
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-14 13:28:52 -0300

    Nono Exercício

---
+++ ExercicioNove.java
@@ -0,0 +1,25 @@
+public class ExercicioNove {
+
+    public static void executar() {
+
+        /* Leia dois números nas variáveis A e B e identifique se os valores são iguais ou
+        diferentes. Caso eles sejam iguais imprima uma mensagem dizendo que são iguais. 
+        Caso sejam diferentes, informe que são diferentes e qual número é o maior. */
+
+        float a = Prompt.lerDecimal("Digite o primeiro número: ");
+        float b = Prompt.lerDecimal("Digite o segundo número: ");
+
+        if (a == b) {
+            System.out.println("Os dois números são iguais!");
+        } else {
+            if (a > b) {
+                System.out.printf("Os dois números são diferentes, sendo %f o maior.\n", a);
+            } else if (b > a) {
+                System.out.printf("Os dois números são diferentes, sendo %f o maior.\n", b);
+            }
+        }
+
+        Prompt.separador();
+        
+    }
+}
