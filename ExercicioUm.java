0ddc6943b529b36f33913ef7c763e6083fe573c2
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-14 13:22:08 -0300

    Primeiro commit

---
+++ ExercicioUm.java
@@ -0,0 +1,21 @@
+public class ExercicioUm {
+
+    public static void executar() {
+
+        /* Escreva um algoritmo que leia um número digitado pelo usuário e mostre 
+        a mensagem “Número maior do que 10!”, caso este número seja maior, ou “Número menor ou 
+        igual a 10!”, caso este número seja menor ou igual */
+    
+    int numero = Prompt.lerInteiro("Digite um número: ");
+
+    if (numero > 10) {
+        System.out.println("Número maior que 10!");
+    } else {
+        System.out.println("Número menor ou igual a 10!");
+    }
+
+    Prompt.separador();
+    
+    }
+
+}
---
+++ Programa.java
@@ -0,0 +1,11 @@
+public class Programa {
+
+    public static void main(String[] args) {
+
+        ExercicioUm.executar();
+        ExercicioDois.executar();
+        ExercicioTres.executar();
+        
+    }
+
+}
