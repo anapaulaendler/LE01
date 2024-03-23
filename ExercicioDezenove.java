c51940da275727c9ab915d221df70e66282e2852
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-22 21:21:34 -0300

    Exercício Dezenove

---
+++ ExercicioDezenove.java
@@ -0,0 +1,14 @@
+public class ExercicioDezenove {
+    public static void executar() {
+        /* Faça um algoritmo que receba o raio e a altura de um cilindro e retorne o seu 
+        volume calculado de acordo com a seguinte fórmula: volume = 3.14 * raio2 * altura; 
+        Exemplo: raio = 10, altura = 15. Volume = 4710  */
+
+        double raio = Prompt.lerDecimal("Digite o raio: ");
+        double altura = Prompt.lerDecimal("Digite a altura: ");
+
+        double volume = 3.14 * raio * raio * altura;
+        System.out.printf("O volume é de: %.2f\n", volume);
+        
+    }
+}
--- Programa.java
+++ Programa.java
@@ -1,24 +1,7 @@
 public class Programa {
 
     public static void main(String[] args) {
-        ExercicioUm.executar();
-        ExercicioDois.executar();
-        ExercicioTres.executar();
-        ExercicioQuatro.executar();
-        ExercicioCinco.executar();
-        ExercicioSeis.executar();
-        ExercicioSete.executar();
-        ExercicioOito.executar();
-        ExercicioNove.executar();
-        ExercicioDez.executar();
-        ExercicioOnze.executar();
-        ExercicioDoze.executar();
-        ExercicioTreze.executar();
-        ExercicioQuatorze.executar();
-        ExercicioQuinze.executar();
-        ExercicioDezesseis.executar();
-        ExercicioDezessete.executar();
-        ExercicioDezoito.executar();
+        ExercicioDezenove.executar();
     }
 
 }
