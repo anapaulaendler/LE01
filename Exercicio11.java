870d980e05a5f899603add31ccf31e1bd0e22fec
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-25 12:11:40 -0300

    Correção

--- Exercicio11.java
+++ Exercicio11.java
@@ -4,29 +4,29 @@ public class Exercicio11 {
 
         /* Escreva um algoritmo que leia três valores inteiros distintos e escreva-os em ordem crescente. */
 
-        double a = Prompt.lerDecimal("Digite seu primeiro número: ");
-        double b = Prompt.lerDecimal("Digite seu segundo número: ");
-        double c = Prompt.lerDecimal("Digite seu terceiro número: ");
+        int a = Prompt.lerInteiro("Digite seu primeiro número: ");
+        int b = Prompt.lerInteiro("Digite seu segundo número: ");
+        int c = Prompt.lerInteiro("Digite seu terceiro número: ");
 
         if (a == b || a == c || b == c ) {
             System.out.println("Números iguais presentes.");
         } else if (a > b && a > c) {
             if (b > c) {
-                System.out.printf("Em ordem crescente: %.2f, %.2f. e %.2f.\n", c, b, a);
+                System.out.printf("Em ordem crescente: %d, %d. e %d.\n", c, b, a);
             } else {
-                System.out.printf("Em ordem crescente: %.2f, %.2f. e %.2f.\n", b, c, a);
+                System.out.printf("Em ordem crescente: %d, %d. e %d.\n", b, c, a);
             }
         } else if (b > a && b > c) {
             if (a > c) {
-                System.out.printf("Em ordem crescente: %.2f, %.2f. e %.2f.\n", c, a, b);
+                System.out.printf("Em ordem crescente: %d, %d. e %d.\n", c, a, b);
             } else {
-                System.out.printf("Em ordem crescente: %.2f, %.2f. e %.2f.\n", a, c, b);
+                System.out.printf("Em ordem crescente: %d, %d. e %d.\n", a, c, b);
             }
         } else {
             if (b > a) {
-                System.out.printf("Em ordem crescente: %.2f, %.2f. e %.2f.\n", a, b, c);
+                System.out.printf("Em ordem crescente: %d, %d. e %d.\n", a, b, c);
             } else {
-                System.out.printf("Em ordem crescente: %.2f, %.2f. e %.2f.\n", b, a, c);
+                System.out.printf("Em ordem crescente: %d, %d. e %d.\n", b, a, c);
             }
         }
 
